9a32da95a91f6aa704144846ef438799e6b693cd
William  McWhorter <a.statistic@Williams-MacBook-Pro.local>
2022-10-11 18:26:23 -0500

    initial commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '12c16961admsh351777b1bbf8840p1439dcjsn41ea0574de7f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "8e54b7e11f45daea6bd07c3d445f6e1d"
